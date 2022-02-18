41bdd5d74dccf19c82248f6be1df93b28e7f84a4
breadbread1984 <breadbread1984@163.com>
2022-02-18 18:34:57 +0800

    update

--- models.py
+++ models.py
@@ -5,20 +5,20 @@ import tensorflow as tf;
 def Attention(output_dim, key_dim = 64, num_head = 4, value_dim = 64, use_nonbatched_bias = False):
   assert key_dim % num_head == 0;
   assert value_dim % num_head == 0;
-  key_dim = key_dim // num_head;
-  value_dim = value_dim // num_head;
   q_data = tf.keras.Input((None, key_dim)); # q_data.shape = (batch, N_queries, q_channels)
   m_data = tf.keras.Input((None, key_dim)); # m_data.shape = (batch, N_keys, m_channels)
   bias = tf.keras.Input((None, None)); # bias.shape = (batch, N_queries, N_keys)
   if use_nonbatched_bias:
     nonbatched_bias = tf.keras.Input((None,)); # nonbatched_bias.shape = (N_queries, N_keys)
+  key_dim = key_dim // num_head;
+  value_dim = value_dim // num_head;
   q = tf.keras.layers.Dense(num_head * key_dim, use_bias = False, kernel_initializer = tf.keras.initializers.GlorotUniform())(q_data); # q.shape = (batch, N_queries, num_head * key_dim);
   q = tf.keras.layers.Reshape((-1, num_head, key_dim))(q); # q.shape = (batch, N_queries, num_head, key_dim)
   k = tf.keras.layers.Dense(num_head * key_dim, use_bias = False, kernel_initializer = tf.keras.initializers.GlorotUniform())(m_data); # k.shape = (batch, N_keys, num_head * key_dim)
   k = tf.keras.layers.Reshape((-1, num_head, key_dim))(k); # k.shape = (batch, N_keys, num_head, key_dim)
   v = tf.keras.layers.Dense(num_head * key_dim, use_bias = False, kernel_initializer = tf.keras.initializers.GlorotUniform())(m_data); # v.shape = (batch, N_keys, num_head * value_dim)
   v = tf.keras.layers.Reshape((-1, num_head, value_dim))(v); # v.shape = (batch, N_keys, num_head, value_dim)
-  logits = tf.keras.layers.Lambda(lambda x, k: tf.linalg.matmul(tf.transpose(x[0], (0, 2, 1, 3)) / tf.math.sqrt(k), tf.transpose(x[1], (0, 2, 1, 3)), transpose_b = True) + tf.expand_dims(x[2], axis = 1), arguments = {'k': key_dim})([q, k, bias]); # logits.shape = (batch, num_head, N_queries, N_keys)
+  logits = tf.keras.layers.Lambda(lambda x: tf.linalg.matmul(tf.transpose(x[0], (0, 2, 1, 3)) / tf.math.sqrt(tf.cast(tf.shape(x[0])[-1], dtype = tf.float32)), tf.transpose(x[1], (0, 2, 1, 3)), transpose_b = True) + tf.expand_dims(x[2], axis = 1))([q, k, bias]); # logits.shape = (batch, num_head, N_queries, N_keys)
   if use_nonbatched_bias:
     logits = tf.keras.layers.Lambda(lambda x: x[0] + tf.reshape(x[1], (1,1,tf.shape(x[1])[0],tf.shape(x[1])[1])))([logits, nonbatched_bias]); # logits.shape = (batch, num_head, N_queries, N_keys)
   weights = tf.keras.layers.Softmax()(logits); # weights.shape = (batch, num_head, N_queries, N_keys)
